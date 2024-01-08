4030e85b9ceb89c1dc1fd59bc01d6cb01f2fb5b6
Savior135 <srivastava.amankumar03@gmail.com>
2024-01-08 11:18:03 +0530

    added app.py

--- src/utils.py
+++ src/utils.py
@@ -50,5 +50,13 @@ def evaluate_models(X_train, y_train,x_test,y_test,models,param):
 
     except Exception as e:
         raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return joblib.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
 
     
